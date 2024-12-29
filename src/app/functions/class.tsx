3d9cf8ee8533db9d9bb5a7599b87ab90b8deda17
mohammadsarraf <108194180+mohammadsarraf@users.noreply.github.com>
2024-12-29 17:33:56 -0500

    Update class.tsx

--- src/app/functions/class.tsx
+++ src/app/functions/class.tsx
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCSlmOX-6AbFI-YIDLzmyDL4WXFVpyT2i4",
-  authDomain: "teamclone-cd356.firebaseapp.com",
-  projectId: "teamclone-cd356",
-  storageBucket: "teamclone-cd356.firebasestorage.app",
-  messagingSenderId: "423064596545",
-  appId: "1:423064596545:web:9b549ce43726012c5496c0",
-  measurementId: "G-YYM2JVLV8M"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCSlmOX-6AbFI-YIDLzmyDL4WXFVpyT2i4",
+//   authDomain: "teamclone-cd356.firebaseapp.com",
+//   projectId: "teamclone-cd356",
+//   storageBucket: "teamclone-cd356.firebasestorage.app",
+//   messagingSenderId: "423064596545",
+//   appId: "1:423064596545:web:9b549ce43726012c5496c0",
+//   measurementId: "G-YYM2JVLV8M"
+// };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// // // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);

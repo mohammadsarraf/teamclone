d5705b859f4b563ea3c4a8dda6c898bc52dab1cc
mohammadsarraf <108194180+mohammadsarraf@users.noreply.github.com>
2025-01-03 18:01:21 -0500

    fixed the color bug
    after the clean up the colors sttoped workig. fixed the issue

--- src/app/components/Toolbar.tsx
+++ src/app/components/Toolbar.tsx
@@ -218,15 +218,14 @@ const Toolbar: React.FC<ToolbarProps> = ({
           {showDropdown === "color" && (
             <div className="absolute left-0 top-full mt-1 w-full rounded border bg-gray-200 shadow-lg">
               <div className="flex flex-col text-sm">
-                {["red-700", "blue-500", "green-500", "white"].map((color) => (
+                {["text-red-700", "text-blue-500", "text-green-500", "text-white"].map((color) => (
                   <div
                     key={color}
                     className="flex cursor-pointer items-center p-2 hover:bg-white"
-                    onClick={() => handleColorClick(`text-${color}`)}
+                    onClick={() => handleColorClick(`${color}`)}
                   >
-                    <FaCircle className={`mr-2 text-${color}`} />{" "}
-                    {color.split("-")[0].charAt(0).toUpperCase() +
-                      color.split("-")[0].slice(1)}
+                    <FaCircle className={`mr-2 ${color}`} />{" "}
+                    {color.split("-")[1].charAt(0).toUpperCase()+ color.split("-")[1].slice(1)}
                   </div>
                 ))}
               </div>

98c8a06852545bf48c02a096ad5dcee0662b6a49
mohammadsarraf <108194180+mohammadsarraf@users.noreply.github.com>
2025-01-07 22:54:50 -0500

    preview overflow issue

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             <MdOutlinePhoneIphone className="text-gray-500 hover:text-gray-700" />
           </div>
         </div>
-        <div className="m-10 flex-1 rounded-lg">
+        <div className="m-10 flex-1 rounded-lg overflow-auto">
           <div
             className={`flex size-full overflow-auto rounded-lg ${colors.bgColor}`}
           >
